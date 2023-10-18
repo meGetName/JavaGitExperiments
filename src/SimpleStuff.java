37ec2b4802cce7d843f4d21cbe4637f74a95e2fd
Michael Goesswein <michael.goesswein@uni-due.de>
2023-10-18 21:43:17 +0200

    First Committ

---
+++ src/SimpleStuff.java
@@ -0,0 +1,11 @@
+
+public class SimpleStuff {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a = 3;
+		 int b = 7;
+		 System.out.println(Integer.toString(a+b));
+	}
+
+}
